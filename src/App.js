a15ebdd26a4d509b04fd451a2c8fe0703bc9e70e
Manoj KS <manojks.062001@gmail.com>
2024-02-27 00:53:16 +0530

    test555

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import './App.css';
+
+function App() {
+  const [username , setUserName]=useState('');
+  const [password , setPassword]=useState('');
+  const [waring1 , setWarning1] = useState(false)
+  const [waring2 , setWarning2] = useState(false)
+  const [welcome , setWelcome] = useState(false)
+  const [error , setError]= useState('')
+  // console.log(username)
+  // console.log(password)
+
+  const handleSubmit = (e)=>{
+   e.preventDefault();
+   if(username===''){
+     setWarning1(true)
+   }
+
+   if(password===''){
+    setWarning2(true)
+  }
+
+  if(username === 'user' && password === 'password'){
+    setWelcome(true)
+  }else{
+    setError('Invaild username or password ')
+    setWelcome(false)
+  }
+
+  }
+
+  return (
+    <div className="App">
+      <h1>Login Page</h1>
+      {welcome ? <p>Welcome, {username}!</p> : 
+        <form onSubmit={handleSubmit}> 
+        <p>{error}</p>
+        <label>Username:</label><input value={username} onChange={(e)=>setUserName(e.target.value)} placeholder='username'></input>
+        {waring1 && <p style={{ color: 'red' }}>Please fill out this feild</p>}<br/>
+        <label>Password:</label><input value={password} onChange={(e)=>setPassword(e.target.value)} placeholder='password'></input>
+        {waring2 && <p style={{ color: 'red' }}>Please fill out this feild</p>}<br/>
+        <button type='submit'>Submit</button>
+      </form>}
+
+    </div>
+  );
+}
+
+export default App;
